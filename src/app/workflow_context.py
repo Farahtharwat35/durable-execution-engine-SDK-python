7e0b173ea6373d07fa82fb32f5f7de4d2f4fc756
Farah Tharwat <farah.tharwat1@procore.com>
2025-05-23 22:31:48 +0300

    add WorkflowContext class
    Co-Authored-By: Salma Elsoly <67603984+SalmaElsoly@users.noreply.github.com>

--- src/app/workflow_context.py
+++ src/app/workflow_context.py
@@ -1,3 +1,5 @@
+#@TODO: add the max retry and the retry mechanism in the execute_action method
+
 class WorkflowContext:
     def __init__(self, execution_id: str):
         self.execution_id = execution_id
